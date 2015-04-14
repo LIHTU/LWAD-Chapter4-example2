da35a7155a6510e0518840fa1c644e849f1e75d3
Robin Nelson <robinanelson@gmail.com>
2015-04-13 20:12:14 -0700

    prior to javascript and styling

--- example_2/app.js
+++ example_2/app.js
@@ -4,5 +4,7 @@ var main = function() {
 	window.alert("hello world!");
 };
 
+// uses jQuery to set up the exetuion of the main function once the html doc is fully loaded and ready
 $(document).ready(main);
 
+ 
